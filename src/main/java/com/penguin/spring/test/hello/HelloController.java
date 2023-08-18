1899acff0ac1bce210ae406514a0b983f3ce57ac
leeseayoung <tnfusrltk12@naver.com>
2023-08-18 18:43:50 +0900

    [이세영] 8월18일 테스트 설정 완료!!

---
+++ src/main/java/com/penguin/spring/test/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.penguin.spring.test.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	
+	@ResponseBody
+	@RequestMapping("/hello/test")
+	public String helloworld() {
+		return "HelloWorld";
+	}
+	
+	
+	
+}
