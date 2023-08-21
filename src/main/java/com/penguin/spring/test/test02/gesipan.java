91d794e8ff7b475a8eb80bffe445430491d6c229
Leeseayoung <tnfusrltk12@naver.com>
2023-08-21 23:07:37 +0900

    [이세영] -8월21일 과제 완료!!

---
+++ src/main/java/com/penguin/spring/test/test02/gesipan.java
@@ -0,0 +1,53 @@
+package com.penguin.spring.test.test02;
+
+public class gesipan {
+
+	//title, user, content 
+	private String title;
+	private String user;
+	private String content;
+	
+	public gesipan(String title, String user, String content) {
+		this.title = title;
+		this.user = user;
+		this.content = content;	
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+	@Override
+	public String toString () {
+		return "(title : " + this.title + " user : " + this.user + " content : " + this.content + ")";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/main/java/com/penguin/spring/test/test02/test02.java
+++ src/main/java/com/penguin/spring/test/test02/test02.java
@@ -5,14 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test/test02")
 public class test02 {
 	
 	
-	@RequestMapping("/test/test02/1")
+	@RequestMapping("/1")
 	public List<Map<String, Object>> moveieList() {
 		
 		List<Map<String, Object>> movieList = new ArrayList<>();
@@ -42,25 +45,55 @@ public class test02 {
 		
 		movieList.add(movieMap2);
 		
+		 
+		Map<String, Object> movieMap3 = new HashMap<>();
+		movieMap3.put("rate", "19");
+		movieMap3.put("director", "윤종빈");
+		movieMap3.put("time", "133");
+		movieMap3.put("title", "범죄와의 전쟁 : 나쁜놈들 전성시대");
+		
+		movieList.add(movieMap3);
+		
+		Map<String, Object> movieMap4 = new HashMap<String, Object>();
+		movieMap4.put("rate", "15");
+		movieMap4.put("director", "프란시스 로렌스");
+		movieMap4.put("time", "137");
+		movieMap4.put("title", "헝거게임");
+		movieList.add(movieMap4);
+		
+		
 		return movieList;
 		
-		 // 여기부터 공부 하셈
-//		Map<String, Object> movieMap3 = new HashMap<>();
-//		map3.put("rate", "19");
-//		map3.put("director", "윤종빈");
-//		map3.put("time", "133");
-//		map3.put("title", "범죄와의 전쟁 : 나쁜놈들 전성시대");
-//		
-//		
-//		Map<String, Object> map4 = new HashMap<String, Object>();
-//		map4.put("rate", "15");
-//		map4.put("director", "프란시스 로렌스");
-//		map4.put("time", "137");
-//		map4.put("title", "헝거게임");
+	}
+
+	//게시판 문제
+	@RequestMapping("/2")
+	public List<gesipan> objectResponse() {
 		
+		List<gesipan> gesipanList = new ArrayList<gesipan>();
+		
+		gesipanList.add(new gesipan("안녕하세요 가입인사 드립니다.", "hagulu", "안녕하세요. 가입했어요. 앞으로 잘 부탁 드립니다. 활동 열심히 하겠습니다."));
+		gesipanList.add(new gesipan("헐 대박" , "bada", "오늘 목요일이 었어... 금요일인줄"));
+		gesipanList.add(new gesipan("오늘 데이트 한 이야기 해드릴게요", "dulumary", "...."));
+		
+		return gesipanList;
+		
+	}
+	
+	
+	// ResponseEntity 문제
+	@RequestMapping("/3")
+	public ResponseEntity<gesipan> entityResponse() {
+		gesipan user = new gesipan("안녕하세요 가입인사 드립니다.", "hagulu", "안녕하세요. 가입했어요. 앞으로 잘 부탁 드립니다. 활동 열심히 하겠습니다.");
+		
+		ResponseEntity<gesipan> entity = new ResponseEntity(user, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return entity;		
 		
 		
 	}
+	
+	
 	
 	
 	
